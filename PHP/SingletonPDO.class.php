9704c227d87825a180c47a5a7210066fb3428f26
Soushi888 <Sacha.pignot@gmail.com>
2020-05-19 04:11:56 -0400

    Update SingletonPDO.class.php

--- PHP/SingletonPDO.class.php
+++ PHP/SingletonPDO.class.php
@@ -1,15 +1,14 @@
 <?php
  
-class SingletonPDO
+class SingletonPDO extends PDO
 {
-    private $PDOInstance     = null;    // pointeur vers l'objet PDO instancié dans le constructeur
     private static $instance = null;    // pointeur vers l'objet SingletonPDO instancié dans getInstance()
   
     const DEFAULT_SQL_HOST = 'localhost';
-    const DEFAULT_SQL_DBN  = 'p41';
-    const DEFAULT_SQL_USER = 'root';
-    const DEFAULT_SQL_PASS = '';
-
+    const DEFAULT_SQL_DBN  = 'watt_landing_page';
+    const DEFAULT_SQL_USER = 'spignot';
+    const DEFAULT_SQL_PASS = '%&e1nO&ox@G,';
+    
     /**
      * Méthode privée accessible uniquement par getInstance() 
      * Constructeur d'une instance de SingeltonPDO
@@ -18,11 +17,10 @@ class SingletonPDO
      */
     private function __construct()
     {
-        $this->PDOInstance = new PDO('mysql:host='.self::DEFAULT_SQL_HOST.';dbname='.self::DEFAULT_SQL_DBN,
+        parent::__construct('mysql:host='.self::DEFAULT_SQL_HOST.';dbname='.self::DEFAULT_SQL_DBN,
                                      self::DEFAULT_SQL_USER,
                                      self::DEFAULT_SQL_PASS);
-        $this->PDOInstance->exec("SET NAMES UTF8");
-        $this->PDOInstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+  
     }
   
     private function __clone (){}  // portée private pour empêcher le clonage 
@@ -38,19 +36,9 @@ class SingletonPDO
         if(is_null(self::$instance))
         {
             self::$instance = new SingletonPDO();
+            self::$instance->exec("SET NAMES UTF8");
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         return self::$instance;
     }
- 
-    /**
-     * Méthode magique __call() qui récupère les appels de méthodes inaccessibles
-     * Ces méthodes sont en fait des méthodes de l'objet PDO, qu'il faut exécuter
-     * @param $name = nom de la méthode inaccessible,
-     *        $params = paramètres associés à l'appel de cette méthode    
-     * @return retour de la méthode de l'instance PDO
-     */
-	function __call($name, $params) {
-        return $this->PDOInstance->$name(...$params);
-    }
-
 }
