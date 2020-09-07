e68180124e2dfe6c032ffbf3e9a7ca4c546799bb
Soushi888 <info@sacha-pignot.com>
2020-09-07 17:46:05 -0400

    ajout d'un script de modal

---
+++ js/modal.js
@@ -0,0 +1,49 @@
+class Modal {
+    constructor() {
+        let modalElement = $("body").append(`<div class="modal">
+            <div class="modal-content">
+                <span class="close-button">&times;</span>
+            </div>
+        </div>`);
+    }
+
+    /**
+     * Ouverture d'une fenêtre modal
+     */
+    static showModal() {
+        let modal = $(".modal");
+
+        modal.addClass("show-modal");
+
+        $(".close-button").on("click", Modal.closeModal);
+
+        $("body").on("keydown", evt => {
+            if (evt.key === "Escape") {
+                Modal.closeModal();
+            }
+        });
+
+        modal.on("click", evt => {
+            if ($(evt.target).hasClass("modal")) {
+                Modal.closeModal();
+            }
+        });
+    }
+
+    /**
+     * Fermeture d'une fenêtre modal
+     */
+    static closeModal() {
+        $(".modal").removeClass("show-modal");
+        $(".modal-content").html("<span class='close-button'>&times;</span>");
+    }
+
+    /**
+     * Réinitialisation du contenu du modal
+     */
+    static resetModal() {
+        $(".modal-content").html("<span class='close-button'>&times;</span>");
+
+        $(".close-button").on("click", Modal.closeModal);
+    }
+}
